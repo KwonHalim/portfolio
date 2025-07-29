64817c47157b5c4a69a3fff1adcf499a6382325a
KwonHalim <hr_blizzard@naver.com>
2025-07-29 23:28:35 +0900

    :sparkles: [Feature] #5 RAG(문서 업로드-임베딩) 과정까지 완료
    임베딩 과정(문서 업로드부터 임베딩까지) 완료된 과정입니다. 각 인터페이스 역할을 하는 클래스들을 주입받아 서비스를 실행합니다.

--- AI/service/rag_service.py
+++ AI/service/rag_service.py
@@ -1,15 +1,22 @@
 import json
 
+from AI.database.vector.repository import VectorRepository
 from AI.service.chunk.service import ChunkService
 from AI.service.embedding.service import EmbeddingService
 from AI.service.rag.data_processor import DataProcessor
 
 
 class RAGService:
-    def __init__(self, chunk_service: ChunkService, embedding_service: EmbeddingService, data_processor: DataProcessor):
+    def __init__(self,
+                 chunk_service: ChunkService,
+                 embedding_service: EmbeddingService,
+                 data_processor: DataProcessor,
+                 vector_repository: VectorRepository,
+                 ):
         self.chunk_service = chunk_service
         self.embedding_service = embedding_service
         self.data_processor = data_processor
+        self.repository = vector_repository
         print("✅ RAGService 초기화 완료")
 
     def process(self, paragraph_data: str | None, qa_data: str | None):
@@ -21,19 +28,24 @@ class RAGService:
         docs.extend(self.data_processor.process_qa_json(qa_data))
         print(f"--- 문서 변환 완료 ---")
         print(f"--- 변환된 문서 개수: {len(docs)} ---")
-        # 2. 청킹 데이터 임베딩
+        # 2. 문서 청킹
         print("--- 문서 청킹 시작 ---")
-        documents = self.chunk_service.chunk_documents(docs)
+        documents = self.chunk_service.split_documents(docs)
         print(f"--- 청킹 완료 ---")
         print(f"--- 생성된 청크 문서 개수: {len(documents)} ---")
-        # 3. 청킹된 문서 임베딩
-        print("--- 청크 문서 임베딩 시작 ---")
-        embedded_documents = self.embedding_service.embed_documents([doc.page_content for doc in documents])
-        print(f"--- 청크 문서 임베딩 완료 ---")
-        print(f"--- 임베딩된 청크 문서 개수: {len(embedded_documents)} ---")
+        ''' 3. 청킹된 문서 임베딩
+        # ChromaDB는 자동 임베딩됨 따라서 주석처리한다.
+        # print("--- 청크 문서 임베딩 시작 ---")
+        # embedded_documents = self.embedding_service.embed_documents([doc.page_content for doc in documents])
+        # print(f"--- 청크 문서 임베딩 완료 ---")
+        # print(f"--- 임베딩된 청크 문서 개수: {len(embedded_documents)} ---")
         # 4. 임베딩된 문서 저장
+        '''
         print("--- 청크 문서 저장 시작 ---")
-        self.chunk_service.save_chunks(documents, embedded_documents)
+        # 3.청크 문서 저장 및 임베딩
+        self.repository.add_documents(documents)
+
+
         print("--- 청크 문서 저장 완료 ---")
         print(f"--- 총 처리된 문서 개수: {len(docs)} ---")
 
