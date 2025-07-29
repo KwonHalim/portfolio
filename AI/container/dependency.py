eb58f0e9536ff6ba730baf4ede346d12d4b4c237
KwonHalim <hr_blizzard@naver.com>
2025-07-29 23:27:29 +0900

    :sparkles: [Feature] #5 의존성 추가 (가장 중요 파일)
    모든 의존성을 관리하는 컨테이너 파일입니다. 해당 파일에서 어떤 함수를, 객체를, 모델을 주입할지 관리가 가능합니다.

--- AI/container/dependency.py
+++ AI/container/dependency.py
@@ -1,3 +1,10 @@
+from typing import Any, Coroutine
+
+from AI.config import settings
+from AI.database.vector.repository import VectorRepository
+from AI.database.vector.vector_strategy.chroma_vector_store import ChromaVectorStore
+from AI.database.vector.vector_strategy.pg_vector_store import PGVectorStore
+from AI.database.vector.vector_strategy.vector_store_strategy import VectorStoreStrategy
 from AI.service.chunk.chunk_strategy.chunk_strategy import ChunkStrategy
 from AI.service.chunk.chunk_strategy.recursive_character_splitter import RecursiveCharacterSplitter
 from AI.service.chunk.service import ChunkService
@@ -17,20 +24,41 @@ async def get_chunk_strategy() -> ChunkStrategy:
 async def get_embedding_strategy() -> EmbeddingStrategy:
     return GoogleGeminiEmbedding(model_name="gemini-embedding-001")
 
-async def get_data_processor() -> 'DataProcessor':
+# 데이터 변환 객체
+async def get_data_processor() -> DataProcessor:
     return DataProcessor()
 
-# --- 중간 제품(하위 서비스) 생성 ---
+# 어떤 DB를 사용할지에 대한 설정
+async def get_vector_store_strategy() -> VectorStoreStrategy:
+    embedding_strategy = await get_embedding_strategy()
+
+    if settings.VECTOR_DB_TYPE == "pgvector":
+        return PGVectorStore(
+            connection_string=settings.PGVECTOR_DB_URL,
+            embedding_strategy=embedding_strategy
+        )
+    elif settings.VECTOR_DB_TYPE == "chroma":
+        return ChromaVectorStore(
+            host=settings.CHROMA_HOST,
+            port=settings.CHROMA_PORT,
+            embedding_strategy=embedding_strategy
+        )
+    else:
+        raise ValueError(f"지원하지 않는 DB 타입입니다: {settings.VECTOR_DB_TYPE}")
+
 async def get_chunk_service() -> ChunkService:
     return ChunkService(chunk_strategy=await get_chunk_strategy())
 
 async def get_embedding_service() -> EmbeddingService:
     return EmbeddingService(embedding_model=await get_embedding_strategy())
 
+async def get_vector_repository() -> VectorRepository:
+    return VectorRepository(vector_store_strategy=await get_vector_store_strategy())
+
 
 async def get_rag_service() -> RAGService:
-    return RAGService(
-        chunk_service=await get_chunk_service(),
-        embedding_service=await get_embedding_service(),
-        data_processor=await get_data_processor()  # DataProcessor는 RAGService 내부에서 초기화됨
-    )
+    return RAGService(chunk_service=await get_chunk_service(),
+                      embedding_service=await get_embedding_service(),
+                      data_processor=await get_data_processor(),
+                      vector_repository=await get_vector_repository(),
+                      )
