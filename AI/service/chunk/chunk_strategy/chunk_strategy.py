b16ca5a3a59caf87731fb6a39c4914ff42d97ed4
KwonHalim <hr_blizzard@naver.com>
2025-07-29 23:24:24 +0900

    :sparkles: [Feature] #5 청크서비스 추가
    chunk폴더에 있는 ChunkService는 청크 관련 서비스 로직이 모여있습니다. 해당 클래스는 청크 관련 함수를 실행하며 chunk_strategy.py 라는 클래스를 주입받아 해당 코드의 함수를 실행가능합니다. 즉 청크 서비스를 실행할때 추가해야하는 로직이 있다면 해당 클래스에 작성하면 됩니다. 또한 해당 서비스는 추후 RagService에서 활용됩니다.

--- AI/service/chunk/chunk_strategy/chunk_strategy.py
+++ AI/service/chunk/chunk_strategy/chunk_strategy.py
@@ -5,7 +5,7 @@ from langchain_core.documents import Document
 
 
 # ABC: Abstract Base Class, 추상메서드클래스로 반드시 따라해야하는 규칙
-# 해당 클래스는 청킹 전략을 정의하는 추상 클래스입니다.
+# 해당 클래스는 청킹 전략을 정의하는 추상 클래스입니다. 인터페이스의 역할
 class ChunkStrategy(ABC):
     @abstractmethod
     def split_documents(self, documents: List[Document]) -> List[Document]: #Document는 Langchain에서 제공하는 문서 클래스
--- AI/service/chunk/service.py
+++ AI/service/chunk/service.py
@@ -9,10 +9,12 @@ class ChunkService:
     def __init__(self, chunk_strategy: ChunkStrategy):
         self.chunk_strategy = chunk_strategy
 
-    def chunk_documents(self, documents: List[Document]) -> List[Document]:
+    def split_documents(self, documents: List[Document]) -> List[Document]:
         """
         문서를 분할합니다.
         """
         print("--- 문서 처리 및 청킹 시작 ---")
+        # 이후 주입된 모델의 청킹 시작.
+        # 만약 공통적인 청킹 추가 로직이 필요하다면 여기 작성
         chunks = self.chunk_strategy.split_documents(documents)
         return chunks
