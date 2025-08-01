986892b81b627aa6184c3647b8de77d08b6efc5c
KwonHalim <hr_blizzard@naver.com>
2025-08-01 22:00:51 +0900

    :sparkles: [Feature] CORS 에러 임시 수정

--- AI/main.py
+++ AI/main.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from AI.routes.chat.chat import chat_router
 from AI.routes.document.document import document_router
@@ -8,6 +9,21 @@ app = FastAPI(
     title="CHATBOT",
     description="Rag AI Chatbot ",
 )
+
+origins = [
+"http://localhost:3000",
+"http://localhost:8000",
+]
+
+app.add_middleware(
+CORSMiddleware,
+allow_origins=["*"],
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],
+)
+
+
 
 app.include_router(router=document_router)
 app.include_router(router=chat_router)
