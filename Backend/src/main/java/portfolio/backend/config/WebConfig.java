4d19ee04bbdabbede86dcc618af037a64c32d022
KwonHalim <hr_blizzard@naver.com>
2025-08-13 16:16:57 +0900

    테스트2

--- Backend/src/main/java/portfolio/backend/config/WebConfig.java
+++ Backend/src/main/java/portfolio/backend/config/WebConfig.java
@@ -10,7 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // /api/로 시작하는 모든 경로에 대해
-                // Cloudflare Pages의 모든 미리보기 주소와 최종 도메인을 허용하도록 패턴을 사용합니다.
                 .allowedOriginPatterns(
                         "https://harim.dev"  // 최종 프로덕션 주소 허용
                 )
