f2ce0ed205aba16e46b9cc3401dbb413ca4c3a31
KwonHarim <halim7753@gmail.com>
2025-09-25 16:41:08 +0900

    fix: :bug: 피드백 도착 시 알림 개발
    The sender address is unauthorized오류 해결
    
    fix #16

--- Backend/src/main/java/portfolio/backend/service/EmailService.java
+++ Backend/src/main/java/portfolio/backend/service/EmailService.java
@@ -2,14 +2,19 @@ package portfolio.backend.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import portfolio.backend.exception.ErrorStatus;
+import portfolio.backend.exception.RestApiException;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService {
     private final JavaMailSender mailSender;
 
@@ -17,11 +22,18 @@ public class EmailService {
     private String myMail;
 
     public void sendEmail(String content) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        String text = "새로운 피드백이 있습니다." + content;
-        message.setTo(myMail);
-        message.setSubject("새로운 피드백이 들어왔습니다.");
-        message.setText(text);
-        mailSender.send(message);
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            String text = "새로운 피드백이 있습니다." + content;
+            message.setTo(myMail);
+            message.setFrom(myMail);
+            message.setSubject("새로운 피드백이 들어왔습니다.");
+            message.setText(text);
+            mailSender.send(message);
+        }
+        catch (Exception e) {
+            log.info("이메일 전송에 실패했습니다 사유:"+e.getMessage());
+            throw new RestApiException(ErrorStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
